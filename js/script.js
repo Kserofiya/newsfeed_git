36b19537bc03f2a590c4544a7d0e0703bcf51911
Kseniya <ad50depo@yandex.ru>
2023-07-23 18:46:34 +0300

    add fetch request to real data

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
-const data = { "sources": [{ "id": 1, "name": "meduza" }, { "id": 6, "name": "3dnews" }, { "id": 7, "name": "nytimes" }, { "id": 8, "name": "forbes" }, { "id": 9, "name": "igromania" }, { "id": 10, "name": "buro237" }, { "id": 11, "name": "rusvesna" }, { "id": 12, "name": "7ya" }, { "id": 14, "name": "aif" }, { "id": 15, "name": "gazetaru" }, { "id": 16, "name": "karpov.courses" }], "categories": [{ "id": 1, "name": "tech" }, { "id": 2, "name": "sport" }, { "id": 3, "name": "fashion" }, { "id": 4, "name": "politics" }, { "id": 5, "name": "other" }, { "id": 6, "name": "karpov.courses" }], "items": [{ "id": 379082, "lang": "ru", "date": "Sun Jul 23 2023 07:50:35 GMT+0300 (Moscow Standard Time)", "title": "Ученые предупредили о возможном голоде вследствие климатического кризиса", "description": "Из-за жары люди во всем мире рискуют столкнуться с острой нехваткой продовольствия.", "image": "https://aif-s3.aif.ru/images/033/233/8889b328068b72b06b6fb7efa9a5ed7f.jpg", "source_id": 14, "category_id": 4 }, { "id": 379083, "lang": "ru", "date": "Sun Jul 23 2023 07:50:18 GMT+0300 (Moscow Standard Time)", "title": "Политик из ФРГ Гердт: Украину выбрали как плацдарм для уничтожения славян", "description": "По его мнению, этого добилась англосакская элита — США и Великобритания.", "image": "https://aif-s3.aif.ru/images/033/233/9c3897fb9f160f4bca44b2571d76b40d.JPG", "source_id": 14, "category_id": 5 }, { "id": 379084, "lang": "ru", "date": "Sun Jul 23 2023 07:42:36 GMT+0300 (Moscow Standard Time)", "title": "Украинскую беженку возмутили надписи на русском на станции метро в Праге", "description": "Станцию метро Andel украшают надписи на русском о советско-чехословацкой дружбе.", "image": "https://aif-s3.aif.ru/images/033/233/51dc19fb48b359e6a2086022fa8144c0.jpg", "source_id": 14, "category_id": 4 }, { "id": 379087, "lang": "ru", "date": "Sun Jul 23 2023 07:35:00 GMT+0300 (Moscow Standard Time)", "title": "Сегодня вступили в силу новые ограничения на экспорт оборудования для производства чипов из Японии", "description": "С конца прошлого года власти США говорили с коллегами из Японии и Нидерландов о необходимости синхронизации санкций на поставку оборудования для производства чипов в неблагонадёжные страны, среди которых негласно лидировал Китай. Оба внешнеполитических союзника долго согласовывали ужесточение мер со своими парламентариями, но в итоге расширенные санкции в этой сфере начали действовать в Японии с этого воскресенья.", "image": "https://3dnews.ru/assets/external/illustrations/2023/07/23/1090414/nikon_01.jpg", "source_id": 6, "category_id": 1 }, { "id": 379085, "lang": "ru", "date": "Sun Jul 23 2023 07:18:06 GMT+0300 (Moscow Standard Time)", "title": "Пропавшего в Кемерово 6-летнего мальчика нашли мертвым в водоеме", "description": "На месте происшествия работают следователи.", "image": "https://aif-s3.aif.ru/images/033/233/2ba901c9dae9208007ebb3efff8bf434.jpg", "source_id": 14, "category_id": 5 }, { "id": 379086, "lang": "ru", "date": "Sun Jul 23 2023 06:53:29 GMT+0300 (Moscow Standard Time)", "title": "Врач сообщила об охоте ВСУ на российских военных медиков", "description": "По словам специалиста, также украинские военные обстреливают санитарные экипажи.", "image": "https://aif-s3.aif.ru/images/033/233/ef5c2c07bc51afd7ee59fb37a498d61b.jpg", "source_id": 14, "category_id": 5 }, { "id": 379088, "lang": "ru", "date": "Sun Jul 23 2023 06:49:21 GMT+0300 (Moscow Standard Time)", "title": "Туристы покидают курорты Греции из-за аномальной жары", "description": "Синоптики предупредили о повышении температуры до плюс 50 градусов.", "image": "https://aif-s3.aif.ru/images/033/233/229e228d04f5b53c7ac6cb836fe01204.jpeg", "source_id": 14, "category_id": 4 }, { "id": 379089, "lang": "ru", "date": "Sun Jul 23 2023 06:28:24 GMT+0300 (Moscow Standard Time)", "title": "Премьер Венгрии Орбан обвинил ЕС в «ЛГБТ-наступлении»", "description": "Политик в очередной раз подверг критике нетрадиционные ценности Евросоюза.", "image": "https://aif-s3.aif.ru/images/033/233/7206c6ec40cdb3aa8836b0d75c044e7b.jpg", "source_id": 14, "category_id": 5 }, { "id": 379090, "lang": "ru", "date": "Sun Jul 23 2023 06:27:35 GMT+0300 (Moscow Standard Time)", "title": "В Сети появилось новое фото британского принца Джорджа", "description": "Наследнику престола 22 июля исполнилось 10 лет.", "image": "https://aif-s3.aif.ru/images/033/233/fb19caccafaf34d8a9484190fc34a5db.png", "source_id": 14, "category_id": 4 }, { "id": 379091, "lang": "ru", "date": "Sun Jul 23 2023 06:15:21 GMT+0300 (Moscow Standard Time)", "title": "Российские бойцы отразили атаки штурмовых групп ВСУ на четырех направлениях", "description": "Огневую поддержку украинским войскам обеспечивали танки и артиллерия на закрытых позициях.", "image": "https://aif-s3.aif.ru/images/033/233/3b480d4daac545e6591eb98c496b37fb.jpg", "source_id": 14, "category_id": 5 }, { "id": 379092, "lang": "ru", "date": "Sun Jul 23 2023 06:06:24 GMT+0300 (Moscow Standard Time)", "title": "В Кузбассе пропал шестилетний мальчик", "description": "По факту произошедшего возбуждено уголовное дело.", "image": "https://aif-s3.aif.ru/images/033/233/6d5b04e049648dd4113870df70e1a028.jpg", "source_id": 14, "category_id": 5 }, { "id": 378982, "lang": "ru", "date": "Sun Jul 23 2023 05:47:00 GMT+0300 (Moscow Standard Time)", "title": "Макгрегор подарил жене автомобиль после скандала с изнасилованием", "description": "Фотографии презента Ди Девлин опубликовала в своих соцсетях.", "image": "https://aif-s3.aif.ru/images/033/233/b40f8566585477841569f0a2a9c96d06.jpg", "source_id": 14, "category_id": 5 }, { "id": 378983, "lang": "ru", "date": "Sun Jul 23 2023 05:46:48 GMT+0300 (Moscow Standard Time)", "title": "ВС РФ отразили три атаки ВСУ на запорожском направлении", "description": "Российские военные уничтожили несколько танков, БМП и живую силу противника.", "image": "https://aif-s3.aif.ru/images/033/233/c74f0478f9d6ce474478636a40f5d4c0.jpg", "source_id": 14, "category_id": 5 }, { "id": 378984, "lang": "ru", "date": "Sun Jul 23 2023 05:18:42 GMT+0300 (Moscow Standard Time)", "title": "В России в полтора раза выросли продажи умных гаджетов для животных", "description": "Речь идет об умных кормушках и трекерах для определения местоположения питомца.", "image": "https://aif-s3.aif.ru/images/033/233/5aef57184c34d640f8ddf91bc8373a12.jpg", "source_id": 14, "category_id": 5 }, { "id": 378985, "lang": "ru", "date": "Sun Jul 23 2023 05:15:01 GMT+0300 (Moscow Standard Time)", "title": "В Турции назвали убийство Журавлева попыткой скрыть истину о конфликте", "description": "Профсоюз турецких журналистов выразил соболезнования в связи со смертью военкора.", "image": "https://aif-s3.aif.ru/images/033/233/7bde235ab1a4ff998fa4b7fca549cce7.jpg", "source_id": 14, "category_id": 5 }, { "id": 378986, "lang": "ru", "date": "Sun Jul 23 2023 04:53:35 GMT+0300 (Moscow Standard Time)", "title": "Военком ВСУ оформил фиктивную инвалидность, чтобы не попасть на передовую", "description": "При этом подполковник пытается продержаться в должности для продолжения своей коррупционной деятельности.", "image": "https://aif-s3.aif.ru/images/033/233/7f712cfc1c2597c4920b33e07e6355c7.jpg", "source_id": 14, "category_id": 5 }, { "id": 378987, "lang": "ru", "date": "Sun Jul 23 2023 04:40:19 GMT+0300 (Moscow Standard Time)", "title": "Сухогруз «Инженер Шуткова» сел на мель в Нижегородской области", "description": "Разлива нефтепродуктов не зафиксировано.", "image": "https://aif-s3.aif.ru/images/033/233/fbf133c4ffe1a0dd2e15e9e9e38fcc25.jpg", "source_id": 14, "category_id": 5 }, { "id": 378988, "lang": "ru", "date": "Sun Jul 23 2023 04:23:44 GMT+0300 (Moscow Standard Time)", "title": "ВСУ применили новые термобарические мины в зоне СВО", "description": "Российские военные научились противодействовать такому типу мин.", "image": "https://aif-s3.aif.ru/images/033/233/552e522d7bf640d26f5e09ab0c081a1b.jpg", "source_id": 14, "category_id": 5 }, { "id": 378989, "lang": "ru", "date": "Sun Jul 23 2023 04:21:56 GMT+0300 (Moscow Standard Time)", "title": "The Hill: американские чиновники устали от капризов Зеленского", "description": "Об этом заявил бывший спецпомощник Пентагона Дуглас Маккинан.", "image": "https://aif-s3.aif.ru/images/033/233/f4b15d1bd3452da8d431c9020ee35172.jpg", "source_id": 14, "category_id": 5 }, { "id": 378990, "lang": "ru", "date": "Sun Jul 23 2023 03:45:47 GMT+0300 (Moscow Standard Time)", "title": "Олимпийскую медаль фигуриста Зайцева продали на аукционе за $93,75 тыс.", "description": "Эту медаль он завоевал на Олимпиаде 1980 года в американском Лейк-Плэсиде в паре с Ириной Родниной.", "image": "https://aif-s3.aif.ru/images/033/233/417b45b90e0b0fef2e3985d3cd977466.jpg", "source_id": 14, "category_id": 5 }] }
-
-const mainNews = data.items.slice(0, 3);
-const smallNews = data.items.slice(3, 12);
-
-const mainNewsTemplate = document.getElementById('main-news-item');
-const smallNewsTemplate = document.getElementById('small-article-item');
-const mainNewsContainer = document.querySelector('.articles__big-column');
-const smallNewsContainer = document.querySelector('.articles__small-column');
+let data = null;
 
 const escapeString = (string) => {
     const symbols = {
@@ -20,39 +12,53 @@ const escapeString = (string) => {
     })
 }
 
-mainNews.forEach((item) => {
-    const categoryData = data.categories.find((categoryItem) => categoryItem.id === item.category_id);
-    const sourceData = data.sources.find((sourceItem) => sourceItem.id === item.source_id);
+const renderNews = (categoryId) => {
+    fetch('http://frontend.karpovcourses.net/api/v2/ru/news/' + (categoryId ? categoryId : ''))
+        .then(response => response.json())
+        .then((responseData) => {
+            data = responseData;
 
-    const article = document.createElement('article');
-    article.classList.add('main-article');
-    article.innerHTML = `
-        <div class="main-article__image-container">
-            <img class="main-article__image" src="${encodeURI(item.image)}" alt="newsfeed_img_1">
-        </div>
-        <div class="main-article__content">
-            <span class="article-category main-article__category">${escapeString(categoryData.name)}</span>
-            <h2 class="main-article__title">${escapeString(item.title)}</h2>
-            <p class="main-article__text">${escapeString(item.description)}</p>
-            <span class="article-source main-article__source">${escapeString(sourceData.name)}</span>
-        </div>
-    `;
-    mainNewsContainer.appendChild(article);
-});
+            const mainNews = data.items.slice(1, 4);
+            const mainNewsContainer = document.querySelector('.articles__big-column');
 
-smallNews.forEach((item) => {
-    const sourceData = data.sources.find((sourceItem) => sourceItem.id === item.source_id);
-    const dateData = new Date(item.date).toLocaleDateString('ru-RU', { month: 'long', day: 'numeric' });
+            mainNews.forEach((item) => {
+                const categoryData = data.categories.find((categoryItem) => categoryItem.id === item.category_id);
+                const sourceData = data.sources.find((sourceItem) => sourceItem.id === item.source_id);
+            
+                const article = document.createElement('article');
+                article.classList.add('main-article');
+                article.innerHTML = `
+                    <div class="main-article__image-container">
+                        <img class="main-article__image" src="${encodeURI(item.image)}" alt="newsfeed_img_1">
+                    </div>
+                    <div class="main-article__content">
+                        <span class="article-category main-article__category">${escapeString(categoryData.name)}</span>
+                        <h2 class="main-article__title">${escapeString(item.title)}</h2>
+                        <p class="main-article__text">${escapeString(item.description)}</p>
+                        <span class="article-source main-article__source">${escapeString(sourceData.name)}</span>
+                    </div>
+                `;
+                mainNewsContainer.appendChild(article);
+            });
 
-    const article = document.createElement('article');
-    article.classList.add('small-article');
-    article.innerHTML = `
-        <h2 class="small-article__title">${escapeString(item.title)}</h2>
-        <p class="small-article__caption">
-            <span class="article-date small-article__date">${dateData}</span>
-            <span class="article-source small-article__source">${escapeString(sourceData.name)}</span>
-        </p>
-    `;
-
-    smallNewsContainer.appendChild(article);
-});
+            const smallNews = data.items.slice(3, 12);
+            const smallNewsContainer = document.querySelector('.articles__small-column');
+            
+            smallNews.forEach((item) => {
+                const sourceData = data.sources.find((sourceItem) => sourceItem.id === item.source_id);
+                const dateData = new Date(item.date).toLocaleDateString('ru-RU', { month: 'long', day: 'numeric' });
+            
+                const article = document.createElement('article');
+                article.classList.add('small-article');
+                article.innerHTML = `
+                    <h2 class="small-article__title">${escapeString(item.title)}</h2>
+                    <p class="small-article__caption">
+                        <span class="article-date small-article__date">${dateData}</span>
+                        <span class="article-source small-article__source">${escapeString(sourceData.name)}</span>
+                    </p>
+                `;
+            
+                smallNewsContainer.appendChild(article);
+            });
+        })
+}
